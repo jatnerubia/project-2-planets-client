8f077b3375b9bf7ae28bf0e8a11d9ca97c6a3757
jomar567 <cladojomar881@gmail.com>
2022-12-07 23:34:10 +0800

    jomar igop

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import Jason from "./components/Jason"
+import Jason from "./components/Jason";
+import Jomar from "./components/Jomar";
 
 const App = () => {
     return (
         <div>
             
             <Jason />
+            <Jomar />
         </div>
     )
 }
