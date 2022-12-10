3db2a549a1358276699595bccf0a4afc3aa0ab2b
jomar567 <cladojomar881@gmail.com>
2022-12-10 14:13:27 +0800

    add scss

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js'
-import './index.css';
+import './assets/sass/index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
