12f41ff4eab41395477eaf24a6a51cb0ea2a7221
jasonlerit <jasonlerit077@gmail.com>
2022-12-12 18:05:15 +0800

    added fetch in planet section

--- src/components/PlanetSection.jsx
+++ src/components/PlanetSection.jsx
@@ -1,15 +1,36 @@
 
+import { useEffect, useState } from 'react'
 import PlanetCard from './PlanetCard'
 
 const PlanetSection = () => {
+
+    const [planets, setPlanets] = useState([])
+
+    useEffect(() => {
+        getPlanets()
+    }, [])
+
+    const getPlanets = async() => {
+        const options = {
+            method: 'GET',
+            headers: {
+                'X-RapidAPI-Key': '4dd6b9fd10mshec2b10eff154adap1f7fb2jsn9c8b9815b2ac',
+                'X-RapidAPI-Host': 'planets-info-by-newbapi.p.rapidapi.com'
+            }
+        };
+
+        const result = await fetch('https://planets-info-by-newbapi.p.rapidapi.com/api/v1/planet/list', options);
+        const response = await result.json()
+
+        setPlanets(response)
+    }
+
     return (
         <div className="container">
             <div className="row">
-
-                <PlanetCard />
-                <PlanetCard />
-                <PlanetCard />
-
+                {
+                    planets.map((planet, index) => <PlanetCard key={index} planet={planet} />)
+                }
             </div>
         </div>
     )
