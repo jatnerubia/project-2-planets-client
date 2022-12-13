c160228a128233306984e020fb1e089f07f93850
jasonlerit <jasonlerit077@gmail.com>
2022-12-13 08:33:06 +0800

    updated planet card styles

--- src/components/LandingPage/PlanetCard.jsx
+++ src/components/LandingPage/PlanetCard.jsx
@@ -1,13 +1,13 @@
 
 const PlanetCard = ({ planet }) => {
     return (
-        <div className="col-md-6 col-lg-4">
+        <div className="col-md-6 col-lg-4 d-flex justify-content-center align-items-center p-2">
             <div className="flip-card">
                 <div className="flip-card-inner">
                     <div className="flip-card-front">
                         <img className="img-fluid" src={planet.images[0].url} alt={planet.name} />
                     </div>
-                    <div className="flip-card-back d-flex flex-column justify-content-center align-items-center">
+                    <div className="flip-card-back d-flex flex-column justify-content-center align-items-center p-2">
                         <h5>{planet.name}</h5>
                         <p>{planet.images[0].description}</p>
                         <button type="button">View More</button>
