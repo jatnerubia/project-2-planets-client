422544f9b8990e306be0a1772545bc2001ee16a5
gelai05 <angelamaemorales1105@gmail.com>
2022-12-14 23:21:30 +0800

    additional info footer

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -4,6 +4,23 @@ const FooterSection = () => {
             <div className="row">
                 <div className="col-lg-4">
                 <h5 class="text-uppercase">SpaceVerse</h5>
+                <ul class="list-unstyled mb-0">
+                  <li>
+                    <a class="text-white">About Us</a>
+                  </li>
+                  <li>
+                    <a class="text-white">Contact Us</a>
+                  </li>
+                  <li>
+                    <a class="text-white">FAQS</a>
+                  </li>
+                  <li>
+                    <a class="text-white">Mission</a>
+                  </li>
+                  <li>
+                    <a class="text-white">Vision</a>
+                  </li>
+                </ul>
               
 
                 </div>
