a0b116d3547bb547857f39184ab51f43dc3603bb
gelai05 <angelamaemorales1105@gmail.com>
2022-12-14 23:18:14 +0800

    footer row

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -1,8 +1,16 @@
 const FooterSection = () => {
     return (
-        <div className="bg-dark">
-            <h1>Footer Section </h1>
+        <div className="container bg-dark ">
+            <div className="row">
+                <div className="col-lg-4">
+                <h5 class="text-uppercase">SpaceVerse</h5>
+              
+
+                </div>
+            </div>
         </div>
     )
 }
 export default FooterSection
+
+"https://i.ibb.co/9qGHSdv/Blue-White-Minimalist-Mini-Rocket-Logo-Design-2.png" 
--- src/components/home/FooterSection.jsx
+++
@@ -1,8 +0,0 @@
-const FooterSection = () => {
-    return (
-        <div className="bg-dark">
-            <h1>Footer Section </h1>
-        </div>
-    )
-}
-export default FooterSection
