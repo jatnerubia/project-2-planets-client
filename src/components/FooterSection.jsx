6a64d2a65198f6bc2198ec51028729312fed3654
gelai05 <angelamaemorales1105@gmail.com>
2022-12-15 21:28:17 +0800

    2nd col footer

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -4,7 +4,29 @@ const FooterSection = () => {
             <div className="row">
                 <div className="col-lg-4">
                 <h5 class="text-uppercase">SpaceVerse</h5>
-                <div className="col-lg-3">
+                </div>
+                <div className="col-3">
+
+                <ul class="list-unstyled mb-0">
+                  <li>
+                    <a class="text-white">About Us</a>
+                  </li>
+                  <li>
+                    <a class="text-white">Contact Us</a>
+                  </li>
+                  <li>
+                    <a class="text-white">FAQS</a>
+                  </li>
+                  <li>
+                    <a class="text-white">Mission</a>
+                  </li>
+                  <li>
+                    <a class="text-white">Vision</a>
+                  </li>
+                </ul>
+                </div>
+                 <div className="col-3">
+
                 <ul class="list-unstyled mb-0">
                   <li>
                     <a class="text-white">About Us</a>
@@ -28,7 +50,7 @@ const FooterSection = () => {
 
                 </div>
             </div>
-        </div>
+        
     )
 }
 export default FooterSection
