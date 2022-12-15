66844c040598077a7e4558352fedbaf7f671060b
gelai05 <angelamaemorales1105@gmail.com>
2022-12-15 22:20:19 +0800

    logo image footer

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -3,8 +3,8 @@ const FooterSection = () => {
         <div className="container mt-5">
             <div className="row">
                 <div className="col-3">
-                  <img src="https://i.ibb.co/9qGHSdv/Blue-White-Minimalist-Mini-Rocket-Logo-Design-2.png"/>
-                <h5 class="text-uppercase">SpaceVerse</h5>
+                  <img src="https://i.ibb.co/chfYwwj/Blue-White-Minimalist-Mini-Rocket-Logo-Design-2.png"/>
+                
                 </div>
                 <div className="col-3">
 
