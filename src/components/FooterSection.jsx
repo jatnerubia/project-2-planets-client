fc9f1da7903b5ab12cc8c2cff715d71b1eea9650
gelai05 <angelamaemorales1105@gmail.com>
2022-12-15 21:33:33 +0800

    3rd col footer

--- src/components/FooterSection.jsx
+++ src/components/FooterSection.jsx
@@ -1,6 +1,6 @@
 const FooterSection = () => {
     return (
-        <div className="container bg-dark ">
+        <div className="container style ">
             <div className="row">
                 <div className="col-lg-4">
                 <h5 class="text-uppercase">SpaceVerse</h5>
@@ -45,7 +45,26 @@ const FooterSection = () => {
                   </li>
                 </ul>
                 </div>
-                
+                <div className="col-3">
+
+                <ul class="list-unstyled mb-0">
+                  <li>
+                    <a class="text-white">About Us</a>
+                  </li>
+                  <li>
+                    <a class="text-white">Contact Us</a>
+                  </li>
+                  <li>
+                    <a class="text-white">FAQS</a>
+                  </li>
+                  <li>
+                    <a class="text-white">Mission</a>
+                  </li>
+                  <li>
+                    <a class="text-white">Vision</a>
+                  </li>
+                </ul>
+                </div>
               
 
                 </div>
