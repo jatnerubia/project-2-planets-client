b5d6b187e5b4527bd829f4265bfe96d1305b22db
alandemol2022 <alandemol05@gmail.com>
2022-12-17 12:34:48 +0800

    added navbar border shadow

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import { NavLink } from "react-router-dom"
 const Navbar = () => {
     return (
-        <nav className="navbar navabar-expand-lg d-flex ">
+        <nav className="navbar navabar-expand-lg pt-1 border border-shawdow d-flex ">
             <div className="col-3 text-center ">
                 <img src="https://i.ibb.co/jwRRDKm/IMG-20221215-224029.png" width="20%" />
-                <h6>SpaceVerse</h6>
+                <h6 >SpaceVerse</h6>
             </div>
-            <div className="col-md-3 navbar navabar-expand-lg d-flex justify-content-between">
+            <div className="col-md-4 navbar navabar-expand-lg d-flex justify-content-between">
                 <NavLink to="/">
                     <button className="btn btn-light">Home</button>
                 </NavLink>
