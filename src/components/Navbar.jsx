2dd31bac823fa1b0e16bbb0fd531203846b74cdb
alandemol2022 <alandemol05@gmail.com>
2022-12-17 12:47:39 +0800

    removed shadow

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import { NavLink } from "react-router-dom"
 const Navbar = () => {
     return (
-        <nav className="navbar navabar-expand-lg pt-1 border border-shawdow d-flex ">
+        <nav className="navbar navabar-expand-lg pt-1 d-flex ">
             <div className="col-3 text-center ">
                 <img src="https://i.ibb.co/jwRRDKm/IMG-20221215-224029.png" width="20%" />
                 <h6 >SpaceVerse</h6>
             </div>
-            <div className="col-md-4 navbar navabar-expand-lg d-flex justify-content-between">
+            <div className="col-md-4 navbar navabar-expand-lg d-flex justify-content-end">
                 <NavLink to="/">
                     <button className="btn btn-light">Home</button>
                 </NavLink>
@@ -14,7 +14,7 @@ const Navbar = () => {
                     <button className="btn btn-light">Planets</button>
                 </NavLink>
                 <NavLink to="/about">
-                    <button className="btn btn-light">About</button>
+                    <button className="btn btn-light me-3">About</button>
                 </NavLink>
             </div>
 
