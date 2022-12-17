ef5b5a8dde63e56e41b5e5228febf05fd229c24a
alandemol2022 <alandemol05@gmail.com>
2022-12-17 15:58:40 +0800

    alligned navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,11 @@ import { NavLink } from "react-router-dom"
 const Navbar = () => {
     return (
         <nav className="navbar navabar-expand-lg pt-1 d-flex ">
-            <div className="col-3 text-center ">
+            <div className="col-md-3 mx-5 d-flex justify-text-center">
                 <img src="https://i.ibb.co/jwRRDKm/IMG-20221215-224029.png" width="30%" />
+                <h4 className="pt-3 mx-3">SpaceVerse</h4>
             </div>
-            <div className="col-m-5">
-                <div className="row">
-                    <h4>SpaceVerse</h4>
-                </div>
-
-            </div>
-            <div className="col-md-4 navbar navabar-expand-lg d-flex justify-text-center">
+            <div className="col-md-3 navbar navabar-expand-lg d-flex justify-text-center">
                 <NavLink to="/">
                     <button className="btn btn-none text-white ">Home</button>
                 </NavLink>
@@ -19,12 +14,12 @@ const Navbar = () => {
                     <button className="btn btn-none text-white">Planets</button>
                 </NavLink>
                 <NavLink to="/about">
-                    <button className="btn btn-none text-white me-5">About</button>
+                    <button className="btn btn-none text-white">About</button>
                 </NavLink>
             </div>
 
-            <div>
-                <button className="col-md-12 btn btn-light me-3 border redius-8">Quiz Game</button>
+            <div className="border-4 me-5">
+                <button className="col-md-9 p-1 btn btn-light me-5">Quiz Game</button>
             </div>
 
         </nav>
