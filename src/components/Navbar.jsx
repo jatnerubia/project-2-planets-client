bd6a9a35ed9aa81292bf5ad6e6c834ef65fada32
alandemol2022 <alandemol05@gmail.com>
2022-12-17 14:05:11 +0800

    removed color home,about,contact

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,25 +3,28 @@ const Navbar = () => {
     return (
         <nav className="navbar navabar-expand-lg pt-1 d-flex ">
             <div className="col-3 text-center ">
-                <img src="https://i.ibb.co/jwRRDKm/IMG-20221215-224029.png" width="30%" />  
+                <img src="https://i.ibb.co/jwRRDKm/IMG-20221215-224029.png" width="30%" />
             </div>
-            <div className="">
-                <h6>SpaceVerse</h6>
+            <div className="col-m-5">
+                <div className="row">
+                    <h4>SpaceVerse</h4>
+                </div>
+
             </div>
-            <div className="col-md-4 navbar navabar-expand-lg d-flex justify-content-between">
+            <div className="col-md-4 navbar navabar-expand-lg d-flex justify-text-center">
                 <NavLink to="/">
-                    <button className="btn btn-light">Home</button>
+                    <button className="btn btn-none text-white ">Home</button>
                 </NavLink>
                 <NavLink to="/planets">
-                    <button className="btn btn-light">Planets</button>
+                    <button className="btn btn-none text-white">Planets</button>
                 </NavLink>
                 <NavLink to="/about">
-                    <button className="btn btn-light">About</button>
+                    <button className="btn btn-none text-white me-5">About</button>
                 </NavLink>
             </div>
 
             <div>
-               <button className="col-md-12 btn btn-light me-3 border redius-8">Quiz Game</button>
+                <button className="col-md-12 btn btn-light me-3 border redius-8">Quiz Game</button>
             </div>
 
         </nav>
