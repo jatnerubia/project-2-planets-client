831f5aaa805b19d4bbfbf5aab3c1a33e89e5eac3
alandemol2022 <alandemol05@gmail.com>
2022-12-17 13:46:57 +0800

    added quiz game button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,10 +3,12 @@ const Navbar = () => {
     return (
         <nav className="navbar navabar-expand-lg pt-1 d-flex ">
             <div className="col-3 text-center ">
-                <img src="https://i.ibb.co/jwRRDKm/IMG-20221215-224029.png" width="20%" />
-                <h6 >SpaceVerse</h6>
+                <img src="https://i.ibb.co/jwRRDKm/IMG-20221215-224029.png" width="30%" />  
             </div>
-            <div className="col-md-4 navbar navabar-expand-lg d-flex justify-content-end">
+            <div className="">
+                <h6>SpaceVerse</h6>
+            </div>
+            <div className="col-md-4 navbar navabar-expand-lg d-flex justify-content-between">
                 <NavLink to="/">
                     <button className="btn btn-light">Home</button>
                 </NavLink>
@@ -14,8 +16,12 @@ const Navbar = () => {
                     <button className="btn btn-light">Planets</button>
                 </NavLink>
                 <NavLink to="/about">
-                    <button className="btn btn-light me-3">About</button>
+                    <button className="btn btn-light">About</button>
                 </NavLink>
+            </div>
+
+            <div>
+               <button className="col-md-12 btn btn-light me-3 border redius-8">Quiz Game</button>
             </div>
 
         </nav>
