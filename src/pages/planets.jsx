4c8424cbe8a7c011365697b966dd6fc450248396
gelai05 <angelamaemorales1105@gmail.com>
2022-12-18 11:21:42 +0800

    adding PlanetHeroSection to Planets Page

--- src/pages/planets.jsx
+++ src/pages/planets.jsx
@@ -1,12 +1,16 @@
 import PlanetDetailsSection from "../components/planets/PlanetDetailsSection";
+import PlanetHeroSection from "../components/planets/PlanetHeroSection";
 
 const PlanetsPage = () => {
   return (
     <>
       <h1 className="text-dark">PLANETS</h1>
 
-      {/* Hero Section */}
+{/* Gelai Testing Planet Hero Section  */}
+      <PlanetHeroSection/>
 
+      {/* Hero Section */}
+      
       <PlanetDetailsSection />
     </>
   );
