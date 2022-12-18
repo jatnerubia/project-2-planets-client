2c73ae90b175cf759b7f9f999d04afd4d45b4f39
gelai05 <angelamaemorales1105@gmail.com>
2022-12-18 12:15:19 +0800

    adding div component

--- src/components/about/Team Section.jsx
+++ src/components/about/Team Section.jsx
@@ -3,9 +3,10 @@ const TeamSection = () => {
         <div className="container">
             <div className ="row">
             <div className="col-3 rounded-image float-start">
-            <img src="https://i.insider.com/569d1bd8e6183e9d408b9e95?width=1000&format=jpeg&auto=webp" alt = "astro" width ="80%"/>
+            <img src="https://i.insider.com/569d1bd8e6183e9d408b9e95?width=1000&format=jpeg&auto=webp" alt = "astro" width ="100%"/>
             </div>
-            <div className="col-9">
+            <div className="col-1"></div>
+            <div className="col-8">
             <h3>Jomar Clado</h3>
             <h6 className="text-justify">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h6>
             </div>
