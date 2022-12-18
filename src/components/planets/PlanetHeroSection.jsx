cdd93fa52f4123509ecaf1301e3ecb289cf72a1b
gelai05 <angelamaemorales1105@gmail.com>
2022-12-18 11:21:52 +0800

    Editing PlanetHero Section

--- src/components/planets/PlanetHeroSection.jsx
+++ src/components/planets/PlanetHeroSection.jsx
@@ -1,9 +1,20 @@
 
 const PlanetHeroSection = () => {
     return (
-        <div>
-            PlanetHeroSection
+        <header className="heroSection__header vh-100">
+      <div className="heroSection__wrapper"></div>
+      <div className="hero-content container text-center">
+        <div className="row">
+          <div className="col-lg-5 text-start">
+            <h1 className="ff-merriweather">PLANETS</h1>
+            <h4 className="lh-base">
+            of the Solar System
+            </h4>
+          </div>
+          <div className="col-lg-7"></div>
         </div>
+      </div>
+    </header>
     )
 }
 export default PlanetHeroSection
