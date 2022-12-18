4fc32c6e47f4c1dc7e867738f6477766f2ae569d
gelai05 <angelamaemorales1105@gmail.com>
2022-12-18 12:21:21 +0800

    adding some margins

--- src/components/about/Team Section.jsx
+++ src/components/about/Team Section.jsx
@@ -24,7 +24,7 @@ const TeamSection = () => {
                 </div>
             </div>
 
-            <div className="row">
+            <div className="row mt-5">
                 <div className="col-3 rounded-image float-start">
                     <img src="https://i.insider.com/569d1bd8e6183e9d408b9e95?width=1000&format=jpeg&auto=webp" alt="astro" width="100%" />
                 </div>
@@ -34,7 +34,7 @@ const TeamSection = () => {
                     <h6 className="text-justify">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h6>
                 </div>
             </div>
-            
+
             <div className="row mt-5">
                 <div className="col-8 rounded-image float-start">
                     <h3>Jason Lerit</h3>
