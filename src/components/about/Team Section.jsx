f78ac3d6d3badc1e4cd1dc54c861dd04f367fc52
gelai05 <angelamaemorales1105@gmail.com>
2022-12-19 17:15:21 +0800

    changing some info

--- src/components/about/Team Section.jsx
+++ src/components/about/Team Section.jsx
@@ -1,14 +1,14 @@
 const TeamSection = () => {
     return (
         
-        <div className="container">
+        <div className="container mx-auto">
             <h1 className="text-center">Meet Our Team</h1>
             <div className="row mt-5">
-                <div className="col-3 rounded-image float-start">
-                    <img src="https://i.insider.com/569d1bd8e6183e9d408b9e95?width=1000&format=jpeg&auto=webp" alt="astro" width="100%" />
+                <div className="col-md-3 col-sm-12 col-3">
+                    <img className ="rounded-circle img-fluid mx-auto" src="https://i.insider.com/569d1bd8e6183e9d408b9e95?width=1000&format=jpeg&auto=webp" alt="astro" width="100%" />
                 </div>
                 <div className="col-1"></div>
-                <div className="col-8">
+                <div className="col-md-8 col-sm-12 col-8">
                     <h3>Jomar Clado</h3>
                     <h6 className="text-justify fs-light">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</h6>
                 </div>
