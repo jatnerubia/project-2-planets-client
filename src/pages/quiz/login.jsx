ef5bbef4bc51e355518b7afc9ee8f12ad5adf0a8
jasonlerit <jasonlerit077@gmail.com>
2022-12-21 21:12:29 +0800

    added login page

---
+++ src/pages/quiz/login.jsx
@@ -0,0 +1,18 @@
+import LoginForm from "../../components/quiz/login/LoginForm"
+
+const Login = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6">
+                    <LoginForm />
+                </div>
+                <div className="col-md-6">
+                    Image Here
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
