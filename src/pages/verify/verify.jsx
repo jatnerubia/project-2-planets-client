0a6c62aee1363c4adbb790e688f61c5109dc4199
jasonlerit <jasonlerit077@gmail.com>
2022-12-22 09:56:54 +0800

    added verify form

--- src/pages/verify/verify.jsx
+++ src/pages/verify/verify.jsx
@@ -1,51 +1,8 @@
-import { useEffect, useState } from "react"
-import { useLocation } from "react-router-dom"
+import VerifyForm from "../../components/verify/VerifyForm"
 
 const VerifyPage = () => {
-
-    const location = useLocation()
-    const [message, setMessage] = useState()
-    
-    useEffect(() => {
-        validateAccount();
-    }, []);
-
-    const validateAccount = async () => {
-        try {
-            const id = new URLSearchParams(location.search).get('id')
-            const token = new URLSearchParams(location.search).get('token')
-            const result = await fetch(
-                `https://project-2-planets-server.onrender.com/verify/account`,
-                {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        id: id,
-                        token: token
-                    })
-                }
-            );
-            let response = await result.json();
-            setTimeout(async () => {
-                if (result.status === 400) {
-                    setMessage(response.message)
-                    return
-                }
-                setMessage(response)
-            }, 3000)
-        } catch (error) {}
-    }
-
     return (
-        <div className="container vh-100 d-flex justify-content-center align-items-center">
-            {
-                message === undefined
-                    ? 'Validating... Please wait.'
-                    : message
-            }
-        </div>
+        <VerifyForm />
     )
 }
 
