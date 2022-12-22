eef7e48e451595638fdba50433722be3f52aab6a
jomar567 <cladojomar881@gmail.com>
2022-12-22 15:10:25 +0800

    fetch jason data to planet card

--- src/components/home/PlanetCard.jsx
+++ src/components/home/PlanetCard.jsx
@@ -4,12 +4,17 @@ const PlanetCard = ({ planet }) => {
   const planetStyles = {
     surface: {
       backgroundImage: `url('${planet.images[0].url}')`,
-      animation: `planetRotate calc(${planet.day}*.1s) linear infinite`
+      animation: `planetRotate calc(${planet.day}*.1s) linear infinite`,
+      transform: `rotate(${planet.tilt}) scale(1.15)`
     },
     atmosphere: {
       boxShadow: `inset 10px 0px 12px -2px rgba(255,255,255,.2),
                   inset -70px 0px 50px 0px black,
                   -5px 0px 10px -4px ${planet.color}`
+    },
+    tiltLine: {
+      transform: `rotate(${planet.tilt}) scale(1.2)`,
+      borderLeft: `1px dashed ${planet.color}`
     }
   }
   return (
@@ -17,6 +22,7 @@ const PlanetCard = ({ planet }) => {
     <div className="planet_card col-lg-4 col-md-6 py-4 my-5">
       <div className="card position-relative mb-4 mx-2">
         <div className="card__planet">
+          <span className="tiltLine" style={planetStyles.tiltLine}></span>
           <div className="planet__atmosphere" style={planetStyles.atmosphere}>
             <div className="planet__surface" style={planetStyles.surface}></div>
           </div>
@@ -38,7 +44,7 @@ const PlanetCard = ({ planet }) => {
               <div className="d-flex align-items-end mb-2">
                 <label>Year</label>
                 <span className="dashed-line"></span>
-                <span>88 Days</span>
+                <span>{planet.year} Days</span>
               </div>
             </div>
             <RedirectButton name="View More" to="/planets" />
