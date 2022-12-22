7e7f385edf84b1d1617e1dfee1b3a423bcbf5061
jomar567 <cladojomar881@gmail.com>
2022-12-22 13:47:01 +0800

    remodify planet section

--- src/components/home/PlanetCard.jsx
+++ src/components/home/PlanetCard.jsx
@@ -1,16 +1,24 @@
 import RedirectButton from "../RedirectButton";
 
 const PlanetCard = ({ planet }) => {
-  const imageCall = {
-    backgroundImage: `url('${planet.images[0].url}')`
+  const planetStyles = {
+    surface: {
+      backgroundImage: `url('${planet.images[0].url}')`,
+      animation: `planetRotate calc(${planet.day}*.1s) linear infinite`
+    },
+    atmosphere: {
+      boxShadow: `inset 10px 0px 12px -2px rgba(255,255,255,.2),
+                  inset -70px 0px 50px 0px black,
+                  -5px 0px 10px -4px ${planet.color}`
+    }
   }
   return (
     <>
     <div className="planet_card col-lg-4 col-md-6 py-4 my-5">
-      <div className="card position-relative bg-white mb-4">
+      <div className="card position-relative bg-white mb-4 mx-4">
         <div className="card__planet">
-          <div className="planet__atmosphere">
-            <div className="planet__surface" style={imageCall}></div>
+          <div className="planet__atmosphere" style={planetStyles.atmosphere}>
+            <div className="planet__surface" style={planetStyles.surface}></div>
           </div>
         </div>
         <div className="card-body text-center text-dark position-absolute w-100">
@@ -20,17 +28,17 @@ const PlanetCard = ({ planet }) => {
               <div className="d-flex align-items-end mb-2">
                 <label>Tilt</label>
                 <span className="dashed-line"></span>
-                <span>0.034°</span>
+                <span>{planet.tilt}</span>
               </div>
               <div className="d-flex align-items-end mb-2">
                 <label>Day</label>
                 <span className="dashed-line"></span>
-                <span>24.6 Hours</span>
+                <span>{planet.day} Hours</span>
               </div>
               <div className="d-flex align-items-end mb-2">
                 <label>Year</label>
                 <span className="dashed-line"></span>
-                <span>687 Days</span>
+                <span>88 Days</span>
               </div>
             </div>
             <RedirectButton name="View More" to="/planets" />
