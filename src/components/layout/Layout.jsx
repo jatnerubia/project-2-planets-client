4b81434196315fdf3b948b6e3ebd11444ce32978
jasonlerit <jasonlerit077@gmail.com>
2022-12-23 13:11:41 +0800

    fixed animated star

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import FooterSection from "../FooterSection";
 import { Outlet } from "react-router-dom";
-import AnimatedStar from "../home/AnimatedStar_bg";
+import AnimatedStar from "../main/home/AnimatedStar";
 import Navbar from "../Navbar";
 
 const Layout = () => {
