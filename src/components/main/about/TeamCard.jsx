a815e0b7cea78e3048c0bd94f0a0f0560edb308a
gelai05 <angelamaemorales1105@gmail.com>
2022-12-26 12:31:01 +0800

    editing some info

--- src/components/main/about/TeamCard.jsx
+++ src/components/main/about/TeamCard.jsx
@@ -1,11 +1,12 @@
 const TeamCard = () => {
     return (
-<div className="container text-center">
-<div className=" col-lg-3 col-sm-6 col-xs-6 col-6">
+<div className="container">
+<div className=" col-lg-6 col-sm-6 col-xs-6 col-6">
   <div className="card  hover-shadow img-fluid card-mb3" >
       <img src="https://t3.ftcdn.net/jpg/02/43/51/48/360_F_243514868_XDIMJHNNJYKLRST05XnnTj0MBpC4hdT5.jpg" className="card-img-top " alt="person"/>
     <div className="card-body">
-      <h3 className="text-black">Alan Demol</h3>
+      <h5 className="text-black text-center">Alan Demol</h5>
+      <p className="text-black text-center">Web Developer</p>
 
     </div>
     
