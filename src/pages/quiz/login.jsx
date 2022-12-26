f1cae64421b9c12c3b0c87f53f25a364190e828f
jomar567 <cladojomar881@gmail.com>
2022-12-26 16:36:06 +0800

    fixed display

--- src/pages/quiz/login.jsx
+++ src/pages/quiz/login.jsx
@@ -4,12 +4,12 @@ const LoginPage = () => {
     return (
         // <div className="container">
             <div className="row form-wrapper h-100">
-                <div className="col-lg-6 col-md-12 p-0">
+                <div className="col-lg-6 col-md-12 d-lg-block d-md-none p-0">
                   <div className="img-wrapper h-100 position-relative">
                     <img src={require("../../assets/img/login.jpg")} className="h-100 position-absolute w-100" alt="" />
                   </div>
                 </div>
-                <div className="col-lg-6 col-md-12 d-lg-block d-md-none p-0">
+                <div className="col-lg-6 col-md-12 p-0">
                     <LoginForm />
                 </div>
             </div>
