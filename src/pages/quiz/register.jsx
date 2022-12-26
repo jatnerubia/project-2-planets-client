79bdd9fbad0bacd29c64952b732805dee4816dc5
jomar567 <cladojomar881@gmail.com>
2022-12-26 16:34:42 +0800

    hide image quiz layout md screen

--- src/pages/quiz/register.jsx
+++ src/pages/quiz/register.jsx
@@ -4,10 +4,10 @@ const RegisterPage = () => {
     return (
         // <div className="container">
             <div className="row form-wrapper h-100">
-                <div className="col-md-6 p-0">
+                <div className="col-lg-6 col-md-12 p-0">
                     <RegisterForm />
                 </div>
-                <div className="col-md-6 p-0">
+                <div className="col-lg-6 col-md-12 d-lg-block d-md-none p-0">
                   <div className="img-wrapper h-100 position-relative">
                     <img src={require("../../assets/img/register.jpg")} className="h-100 position-absolute w-100" alt="" />
                   </div>
