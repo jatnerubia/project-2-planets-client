3916fed97f0046c3babcb73969889d68aa615a99
gelai05 <angelamaemorales1105@gmail.com>
2022-12-26 18:38:01 +0800

    changing pictire

--- src/components/main/about/TeamCard.jsx
+++ src/components/main/about/TeamCard.jsx
@@ -4,8 +4,8 @@ const TeamCard = () => {
         <div className="container ">
             <div className="row">
                 <div className="col-6 col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 ">
-                    <div className="card position-relative " style={{ width: "15rem;" }} >
-                        <img src="https://t3.ftcdn.net/jpg/02/43/51/48/360_F_243514868_XDIMJHNNJYKLRST05XnnTj0MBpC4hdT5.jpg" className="position-absolute img-fluid top-0 start-50 translate-middle mb-5  d-block" alt="person" style={{ height: "50%", float: "auto" }} />
+                    <div className="card position-relative" style={{ width: "15rem;" }} >
+                        <img src="https://i.ibb.co/2c362Xy/Untitled-design-4.png" className="position-absolute img-fluid top-0 start-50 translate-middle mb-5  d-block" alt="person" style={{ height: "50%", float: "auto" }} />
                         <div className="card-body mt-5 pt-5 text-center ">
                             <h5 className="text-black">Alan Demol</h5>
                             <p className="text-black">Web Developer</p>
@@ -17,7 +17,7 @@ const TeamCard = () => {
 
                 <div className="col-6 col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 mb-5">
                     <div className="card position-relative " style={{ width: "15rem;" }} >
-                        <img src="https://t3.ftcdn.net/jpg/02/43/51/48/360_F_243514868_XDIMJHNNJYKLRST05XnnTj0MBpC4hdT5.jpg" className="position-absolute img-fluid top-0 start-50 translate-middle mb-5  d-block" alt="person" style={{ height: "50%", float: "auto" }} />
+                        <img src="https://i.ibb.co/2c362Xy/Untitled-design-4.png" className="position-absolute img-fluid top-0 start-50 translate-middle mb-5  d-block" alt="person" style={{ height: "50%", float: "auto" }} />
                         <div className="card-body mt-5 pt-5 text-center">
                             <h5 className="text-black">Alan Demol</h5>
                             <p className="text-black">Web Developer</p>
@@ -31,7 +31,7 @@ const TeamCard = () => {
             <div className="row mt-5">
                 <div className="col-6 col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 ">
                     <div className="card position-relative " style={{ width: "15rem;" }} >
-                        <img src="https://t3.ftcdn.net/jpg/02/43/51/48/360_F_243514868_XDIMJHNNJYKLRST05XnnTj0MBpC4hdT5.jpg" className="position-absolute img-fluid top-0 start-50 translate-middle mb-5  d-block" alt="person" style={{ height: "50%", float: "auto" }} />
+                        <img src="https://i.ibb.co/2c362Xy/Untitled-design-4.png" className="position-absolute img-fluid top-0 start-50 translate-middle mb-5  d-block" alt="person" style={{ height: "50%", float: "auto" }} />
                         <div className="card-body mt-5 pt-5 text-center">
                             <h5 className="text-black">Alan Demol</h5>
                             <p className="text-black">Web Developer</p>
@@ -43,7 +43,7 @@ const TeamCard = () => {
 
                 <div className="col-6 col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <div className="card position-relative " style={{ width: "15rem;" }} >
-                        <img src="https://t3.ftcdn.net/jpg/02/43/51/48/360_F_243514868_XDIMJHNNJYKLRST05XnnTj0MBpC4hdT5.jpg" className="position-absolute img-fluid top-0 start-50 translate-middle mb-5  d-block" alt="person" style={{ height: "50%", float: "auto" }} />
+                        <img src="https://i.ibb.co/2c362Xy/Untitled-design-4.png" className="position-absolute img-fluid top-0 start-50 translate-middle mb-5  d-block" alt="person" style={{ height: "50%", float: "auto" }} />
                         <div className="card-body mt-5 pt-5 text-center">
                             <h5 className="text-black ">Alan Demol</h5>
                             <p className="text-black">Web Developer</p>
