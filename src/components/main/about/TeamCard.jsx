a543572ad05b8b5f6cf48953b455f29c514b8fdd
gelai05 <angelamaemorales1105@gmail.com>
2022-12-26 11:55:24 +0800

    adding team card section

---
+++ src/components/main/about/TeamCard.jsx
@@ -0,0 +1,19 @@
+const TeamCard = () => {
+    return (
+<div className="container text-center">
+<div className=" col-lg-3 col-sm-6 col-xs-6 col-6">
+  <div className="card  hover-shadow img-fluid card-mb3" >
+      <img src="https://t3.ftcdn.net/jpg/02/43/51/48/360_F_243514868_XDIMJHNNJYKLRST05XnnTj0MBpC4hdT5.jpg" className="card-img-top " alt="person"/>
+    <div className="card-body">
+      <h3 className="text-black">Alan Demol</h3>
+
+    </div>
+    
+    </div></div>
+
+
+
+ </div>
+    )
+}
+export default TeamCard
