69e9fc5f0b7b2fd35d3b13517311be92c69cbb1b
jomar567 <cladojomar881@gmail.com>
2022-12-26 15:48:39 +0800

    fixed login design

--- src/components/layout/QuizLayout.jsx
+++ src/components/layout/QuizLayout.jsx
@@ -2,13 +2,17 @@ import { Outlet } from "react-router-dom"
 
 const QuizLayout = () => {
     return (
-        <>
-            {/* TODO: Quiz Navbar */}
+      <>
+        {/* TODO: Quiz Navbar */}
 
-            <main>
-                <Outlet />
-            </main>
-        </>
+        <main>
+          <div className="quiz d-flex justify-content-center align-items-center p-4">
+            <div className="container position-relative">
+              <Outlet />
+            </div>
+          </div>
+        </main>
+      </>
     )
 }
 
