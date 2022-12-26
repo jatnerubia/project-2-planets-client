d595f050dc69b8fc5ac38cb108dc70ab06254a35
gelai05 <angelamaemorales1105@gmail.com>
2022-12-26 18:29:15 +0800

    center

--- src/components/main/about/TeamCard.jsx
+++ src/components/main/about/TeamCard.jsx
@@ -18,9 +18,9 @@ const TeamCard = () => {
                 <div className="col-6 col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 mb-5">
                     <div className="card position-relative " style={{ width: "15rem;" }} >
                         <img src="https://t3.ftcdn.net/jpg/02/43/51/48/360_F_243514868_XDIMJHNNJYKLRST05XnnTj0MBpC4hdT5.jpg" className="position-absolute img-fluid top-0 start-50 translate-middle mb-5  d-block" alt="person" style={{ height: "50%", float: "auto" }} />
-                        <div className="card-body mt-5 pt-5">
-                            <h5 className="text-black text-center ">Alan Demol</h5>
-                            <p className="text-black text-center">Web Developer</p>
+                        <div className="card-body mt-5 pt-5 text-center">
+                            <h5 className="text-black">Alan Demol</h5>
+                            <p className="text-black">Web Developer</p>
                             <button className="bg-black text-white btn rounded-pill m-2">FOLLOW</button>
                             <button className="border-dark text-black btn rounded-pill">HIRE ME</button>
                         </div>
@@ -32,9 +32,9 @@ const TeamCard = () => {
                 <div className="col-6 col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6 ">
                     <div className="card position-relative " style={{ width: "15rem;" }} >
                         <img src="https://t3.ftcdn.net/jpg/02/43/51/48/360_F_243514868_XDIMJHNNJYKLRST05XnnTj0MBpC4hdT5.jpg" className="position-absolute img-fluid top-0 start-50 translate-middle mb-5  d-block" alt="person" style={{ height: "50%", float: "auto" }} />
-                        <div className="card-body mt-5 pt-5 ">
-                            <h5 className="text-black text-center ">Alan Demol</h5>
-                            <p className="text-black text-center">Web Developer</p>
+                        <div className="card-body mt-5 pt-5 text-center">
+                            <h5 className="text-black">Alan Demol</h5>
+                            <p className="text-black">Web Developer</p>
                             <button className="bg-black text-white btn rounded-pill m-2">FOLLOW</button>
                             <button className="border-dark text-black btn rounded-pill">HIRE ME</button>
                         </div>
@@ -44,9 +44,9 @@ const TeamCard = () => {
                 <div className="col-6 col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
                     <div className="card position-relative " style={{ width: "15rem;" }} >
                         <img src="https://t3.ftcdn.net/jpg/02/43/51/48/360_F_243514868_XDIMJHNNJYKLRST05XnnTj0MBpC4hdT5.jpg" className="position-absolute img-fluid top-0 start-50 translate-middle mb-5  d-block" alt="person" style={{ height: "50%", float: "auto" }} />
-                        <div className="card-body mt-5 pt-5">
-                            <h5 className="text-black text-center ">Alan Demol</h5>
-                            <p className="text-black text-center">Web Developer</p>
+                        <div className="card-body mt-5 pt-5 text-center">
+                            <h5 className="text-black ">Alan Demol</h5>
+                            <p className="text-black">Web Developer</p>
                             <button className="bg-black text-white btn rounded-pill m-2">FOLLOW</button>
                             <button className="border-dark text-black btn rounded-pill">HIRE ME</button>
                         </div>
