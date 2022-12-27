031b9c23d8c676e0e8acc523a62522cc0b5a4e6d
jasonlerit <jasonlerit077@gmail.com>
2022-12-27 16:03:19 +0800

    validate token

--- src/components/layout/UserLayout.jsx
+++ src/components/layout/UserLayout.jsx
@@ -1,6 +1,25 @@
-import { Outlet } from "react-router-dom"
+import { useEffect } from "react"
+import { Outlet, useNavigate } from "react-router-dom"
+import decode from 'jwt-decode'
 
 const UserLayout = () => {
+
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        const token = localStorage.getItem('token')
+        if (!token) {
+            navigate('/quiz')
+            return
+        }
+        const decodedToken = decode(token)
+        if (decodedToken.exp * 1000 < new Date().getTime()) {
+            localStorage.clear()
+            navigate('/quiz')
+            return
+        }
+    }, [])
+
     return (
         <div className="container-fluid">
             <div className="row">
