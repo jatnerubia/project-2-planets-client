2d4579d6ca849a31d3793d730cd099754311a61a
gelai05 <angelamaemorales1105@gmail.com>
2022-12-29 17:38:20 +0800

    added card body

--- src/components/user/profile/UserProfile.jsx
+++ src/components/user/profile/UserProfile.jsx
@@ -5,17 +5,18 @@ const UserProfile = () => {
                 <div className="row text-center">
                     <img src="https://i.ibb.co/2c362Xy/Untitled-design-4.png" className="w-50 img-fluid "></img>
                 </div>
-                <div className="row">
-                    <div className="col">
-                        <label>Name</label>
-                        <input type="text"></input>
-                    </div>
-                    <div className="col">
-                        <label>LastName</label>
-                        <input type="text"></input>
+                <div className="card-body text-center">
+                    <div className="row">
+                        <div className="col-6">
+                            <label>Name</label>
+                            <input type="text"></input>
+                        </div>
+                        <div className="col-6">
+                            <label>LastName</label>
+                            <input type="text"></input>
+                        </div>
                     </div>
                 </div>
-
             </div>
         </div>
     )
