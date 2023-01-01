02f2ada0418a958be0c34ddbdba6b997d1df1079
gelai05 <angelamaemorales1105@gmail.com>
2023-01-01 23:02:31 +0800

    adding quiz hero component

--- src/components/quiz/home/HeroSection.jsx
+++ src/components/quiz/home/HeroSection.jsx
@@ -1,10 +1,19 @@
-const HeroSection = () => {
+const QuizHeroSection = () => {
   return (
-    <div className="quiz-hero_section">
-      <div className="container">
-        <h2>Quiz Hero Section</h2>
+      <div>
+   <header className="heroSection__header vh-100">
+    <div className="heroSection__wrapper"></div>
+    <div className="hero-content container text-center">
+      <div className="row">
+        <div className="col-lg-5 text-start">
+          <h1 className="ff-merriweather">Do you Fully Know The Universe?</h1>
+          <button>Take The Quiz</button>
+        </div>
+        <div className="col-lg-7"></div>
       </div>
     </div>
+  </header>
+      </div>
   )
 }
-export default HeroSection
+export default QuizHeroSection
