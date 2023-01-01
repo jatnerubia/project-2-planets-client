a957362f8357a65fc3d1b2d9888bf4df80a3963a
gelai05 <angelamaemorales1105@gmail.com>
2023-01-01 22:42:33 +0800

    Quiz Hero

--- src/components/user/quiz/QuizHeroSection.jsx
+++ src/components/user/quiz/QuizHeroSection.jsx
@@ -1,7 +1,18 @@
 const QuizHeroSection = () => {
     return (
         <div>
-            Quiz Hero Section
+     <header className="heroSection__header vh-100">
+      <div className="heroSection__wrapper"></div>
+      <div className="hero-content container text-center">
+        <div className="row">
+          <div className="col-lg-5 text-start">
+            <h1 className="ff-merriweather">Do you Fully Know The Universe?</h1>
+            <button>Take The Quiz</button>
+          </div>
+          <div className="col-lg-7"></div>
+        </div>
+      </div>
+    </header>
         </div>
     )
 }
