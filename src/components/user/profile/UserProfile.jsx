09dab4e16ce6d74645b8670e5709606a0b22093c
gelai05 <angelamaemorales1105@gmail.com>
2023-01-01 22:20:29 +0800

    user profile

--- src/components/user/profile/UserProfile.jsx
+++ src/components/user/profile/UserProfile.jsx
@@ -2,18 +2,18 @@ const UserProfile = () => {
     return (
         <div className="container">
             <div className="card">
-                <div className="row text-center">
+                <div className="row justify-content-center">
                     <img src="https://i.ibb.co/2c362Xy/Untitled-design-4.png" className="w-50 img-fluid "></img>
-                </div>
-                <div className="card-body text-center">
-                    <div className="row">
-                        <div className="col-6">
-                            <label>Name</label>
-                            <input type="text"></input>
+                <div className="card-body justify-content-center">
+                    <div className="row align-items-center">
+                        <div className="col-6 ">
+                        <label for="firstName">Firstname</label>
+                        <input type="text" id="firstName"></input>
                         </div>
                         <div className="col-6">
-                            <label>LastName</label>
-                            <input type="text"></input>
+                        <label for="lastName">Lastname</label>
+                        <input type="text" id="lastName"></input>
+                            </div>
                         </div>
                     </div>
                 </div>
