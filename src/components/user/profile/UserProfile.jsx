6e65a419c9f87d7b8f5f149949e2dfdc958abffa
gelai05 <angelamaemorales1105@gmail.com>
2023-01-02 13:38:34 +0800

    user profile

--- src/components/user/profile/UserProfile.jsx
+++ src/components/user/profile/UserProfile.jsx
@@ -1,24 +1,27 @@
 const UserProfile = () => {
     return (
         <div className="container">
-            <div className="card">
-                <div className="row justify-content-center">
-                    <img src="https://i.ibb.co/2c362Xy/Untitled-design-4.png" className="w-50 img-fluid " alt="userImage"></img>
-                <div className="card-body justify-content-center">
-                    <div className="row align-items-center">
-                        <div className="col-6 ">
-                        <label for="firstName">Firstname</label>
-                        <input type="text" id="firstName"></input>
-                        </div>
-                        <div className="col-6">
-                        <label for="lastName">Lastname</label>
-                        <input type="text" id="lastName"></input>
+            <div className="card card-block">
+                <div className="card-body">
+                    <div className="row justify-content-center">
+                        <img src="https://i.ibb.co/2c362Xy/Untitled-design-4.png" className="w-50 img-responsive mb-2" alt="userImage"></img>
+                    </div>
+                    <div className="row justify-content-center">
+                        <div className="col-sm-6 col-md-6 col-lg-6 col-xl-6 d-block">
+                            <label for="firstName">Firstname</label>
+                            <input type="text" id="firstName"></input>
+                            </div>
+                         <div className="col-sm-6 col-md-6 col-lg-6 col-xl-6 d-block">
+                            <label for="lastName">Lastname</label>
+                            <input type="text" id="lastName"></input>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
+                </div>
+            
+       
+
     )
 }
 export default UserProfile
