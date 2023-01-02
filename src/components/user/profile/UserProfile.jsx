d3c9d2aeb9ace0d85b0eccdd896caa7b5d1c1bfe
gelai05 <angelamaemorales1105@gmail.com>
2023-01-02 20:40:13 +0800

    image

--- src/components/user/profile/UserProfile.jsx
+++ src/components/user/profile/UserProfile.jsx
@@ -3,24 +3,24 @@ const UserProfile = () => {
         <div className="container">
             <div className="card card-block">
                 <div className="card-body">
-                    <div className="row justify-content-center">
-                        <img src="https://i.ibb.co/2c362Xy/Untitled-design-4.png" className="w-50 img-responsive mb-2" alt="userImage"></img>
+                    <div className="text-center">
+                        <img src="https://images.pexels.com/photos/109851/pexels-photo-109851.jpeg?auto=compress&cs=tinysrgb&w=150&h=150&dpr=1" className="rounded-circle" alt="User Avatar" width={150} height={150} />
                     </div>
                     <div className="row justify-content-center">
                         <div className="col-sm-6 col-md-6 col-lg-6 col-xl-6 d-block">
                             <label for="firstName">Firstname</label>
                             <input type="text" id="firstName"></input>
-                            </div>
-                         <div className="col-sm-6 col-md-6 col-lg-6 col-xl-6 d-block">
+                        </div>
+                        <div className="col-sm-6 col-md-6 col-lg-6 col-xl-6 d-block">
                             <label for="lastName">Lastname</label>
                             <input type="text" id="lastName"></input>
-                            </div>
                         </div>
                     </div>
                 </div>
-                </div>
-            
-       
+            </div>
+        </div>
+
+
 
     )
 }
