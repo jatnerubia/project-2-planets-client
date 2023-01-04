032d03ee62d3d8e9ecb05226c64d82a8d5047bce
jasonlerit <jasonlerit077@gmail.com>
2023-01-04 12:59:00 +0800

    updated planet card

--- src/components/main/home/PlanetCard.jsx
+++ src/components/main/home/PlanetCard.jsx
@@ -1,6 +1,7 @@
 import RedirectButton from "../RedirectButton";
 
 const PlanetCard = ({ planet }) => {
+
   const planetStyles = {
     surface: {
       backgroundImage: `url('${planet.images[0].url}')`,
@@ -17,41 +18,42 @@ const PlanetCard = ({ planet }) => {
       borderLeft: `1px dashed ${planet.color}`
     }
   }
+
   return (
     <>
-    <div className="planet_card col-lg-4 col-md-6 py-4 my-3">
-      <div className="card position-relative mb-4 mx-2">
-        <div className="card__planet">
-          <span className="tiltLine" style={planetStyles.tiltLine}></span>
-          <div className="planet__atmosphere" style={planetStyles.atmosphere}>
-            <div className="planet__surface" style={planetStyles.surface}></div>
-          </div>
-        </div>
-        <div className="card-body text-center text-white position-absolute w-100">
-          <div className="planet_details">
-            <h3 className="pt-2 pb-4 mb-0">{planet.name}</h3>
-            <div className="d-flex flex-column justify-content-between">
-              <div className="d-flex align-items-end mb-2">
-                <label>Tilt</label>
-                <span className="dashed-line"></span>
-                <span>{planet.tilt}</span>
-              </div>
-              <div className="d-flex align-items-end mb-2">
-                <label>Day</label>
-                <span className="dashed-line"></span>
-                <span>{planet.day} Hours</span>
-              </div>
-              <div className="d-flex align-items-end mb-2">
-                <label>Year</label>
-                <span className="dashed-line"></span>
-                <span>{planet.year} Days</span>
-              </div>
+      <div className="planet_card col-lg-4 col-md-6 py-4 my-3">
+        <div className="card position-relative mb-4 mx-2">
+          <div className="card__planet">
+            <span className="tiltLine" style={planetStyles.tiltLine}></span>
+            <div className="planet__atmosphere" style={planetStyles.atmosphere}>
+              <div className="planet__surface" style={planetStyles.surface}></div>
             </div>
-            <RedirectButton name="View More" to={"/planets#" + planet.name} />
+          </div>
+          <div className="card-body text-center text-white position-absolute w-100">
+            <div className="planet_details">
+              <h3 className="pt-2 pb-4 mb-0">{planet.name}</h3>
+              <div className="d-flex flex-column justify-content-between">
+                <div className="d-flex align-items-end mb-2">
+                  <label>Tilt</label>
+                  <span className="dashed-line"></span>
+                  <span>{planet.tilt}</span>
+                </div>
+                <div className="d-flex align-items-end mb-2">
+                  <label>Day</label>
+                  <span className="dashed-line"></span>
+                  <span>{planet.day} Hours</span>
+                </div>
+                <div className="d-flex align-items-end mb-2">
+                  <label>Year</label>
+                  <span className="dashed-line"></span>
+                  <span>{planet.year} Days</span>
+                </div>
+              </div>
+              <RedirectButton name="View More" to={"/planets#" + planet.name} />
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
