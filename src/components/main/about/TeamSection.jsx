971c2fdd0d1c23c82c7302956f19e0b5ceaeeaa7
alandemol <alandemol05@gmail.com>
2023-01-04 20:26:13 +0800

    updated team card

--- src/components/main/about/TeamSection.jsx
+++ src/components/main/about/TeamSection.jsx
@@ -1,9 +1,20 @@
+import SectionHeading from "../SectionHeading"
+import Teamcard from "./TeamCard"
+
 const TeamSection = () => {
     return (
-             
-               <div>
-                <h3 className="text-center mb-5">Meet the Team</h3>
-               </div>  
+        <div className="py-5">
+            <SectionHeading name="MEET THE TEAM" />
+            <div className="container pt-5">
+                <div className="row gy-5">
+                    <Teamcard firstName="Alan" lastName="Demol" />
+                    <Teamcard firstName="Angela" lastName="Morales" />
+                    <Teamcard firstName="Jason" lastName="Lerit" />
+                    <Teamcard firstName="Jomar" lastName="Clado" />
+                </div>
+            </div>
+        </div>
+
     )
 }
 export default TeamSection
--- src/pages/main/about.jsx
+++ src/pages/main/about.jsx
@@ -1,12 +1,10 @@
 import TeamSection from "../../components/main/about/TeamSection";
-import TeamCard from "../../components/main/about/TeamCard";
 
 const AboutPage = () => {
 
   return (
     <>
       <TeamSection/>
-      <TeamCard/>
     </>
   );
 };
