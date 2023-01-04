ed2c50e17bae595c297c21a59f3191b11f7f0bb0
jasonlerit <jasonlerit077@gmail.com>
2023-01-04 13:04:57 +0800

    updated quiz hero section

--- src/components/quiz/home/QuizHeroSection.jsx
+++ src/components/quiz/home/QuizHeroSection.jsx
@@ -1,19 +1,19 @@
 const QuizHeroSection = () => {
   return (
-      <div>
-   <header className="heroSection__header vh-100">
-    <div className="heroSection__wrapper"></div>
-    <div className="hero-content container text-center">
-      <div className="row">
-        <div className="col-lg-5 text-start">
-          <h1 className="ff-merriweather">Do you Fully Know The Universe?</h1>
-          <button>Take The Quiz</button>
+    <div>
+      <header className="heroSection__header vh-100">
+        <div className="heroSection__wrapper"></div>
+        <div className="hero-content container text-center">
+          <div className="row">
+            <div className="col-lg-5 text-start">
+              <h1 className="ff-merriweather">Do you Fully Know The Universe?</h1>
+              <button>Take The Quiz</button>
+            </div>
+            <div className="col-lg-7"></div>
+          </div>
         </div>
-        <div className="col-lg-7"></div>
-      </div>
+      </header>
     </div>
-  </header>
-      </div>
   )
 }
 export default QuizHeroSection
