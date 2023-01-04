757fc6ecd66acf2f6bb4c969d4f024eff7c84635
jasonlerit <jasonlerit077@gmail.com>
2023-01-04 12:57:27 +0800

    updated news section

--- src/components/main/home/NewsSection.jsx
+++ src/components/main/home/NewsSection.jsx
@@ -4,6 +4,7 @@ import NewsCard from "./NewsCard";
 import * as RestApi from "../../../utils/rest_api_util"
 
 const NewsSection = () => {
+
   const [news, setNews] = useState([]);
 
   useEffect(() => {
@@ -22,9 +23,9 @@ const NewsSection = () => {
     <div className="container news_article__section py-5 my-5">
       <SectionHeading classname="news" name="NEWS" />
       <div className="row py-5">
-        {news.map((news, index) => (
-          <NewsCard key={index} news={news} />
-        ))}
+        {
+          news.map((news, index) => <NewsCard key={index} news={news} />)
+        }
       </div>
     </div>
   )
