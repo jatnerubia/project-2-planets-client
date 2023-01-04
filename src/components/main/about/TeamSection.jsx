efdeed6e05c1941a00f591daefee813e509134fc
alandemol <alandemol05@gmail.com>
2023-01-04 20:31:35 +0800

    updated team section

--- src/components/main/about/TeamSection.jsx
+++ src/components/main/about/TeamSection.jsx
@@ -3,15 +3,13 @@ import Teamcard from "./TeamCard"
 
 const TeamSection = () => {
     return (
-        <div className="py-5">
+        <div className="container">
             <SectionHeading name="MEET THE TEAM" />
-            <div className="container pt-5">
-                <div className="row gy-5">
-                    <Teamcard firstName="Alan" lastName="Demol" />
-                    <Teamcard firstName="Angela" lastName="Morales" />
-                    <Teamcard firstName="Jason" lastName="Lerit" />
-                    <Teamcard firstName="Jomar" lastName="Clado" />
-                </div>
+            <div className="row gy-5 pt-5">
+                <Teamcard firstName="Alan" lastName="Demol" />
+                <Teamcard firstName="Angela" lastName="Morales" />
+                <Teamcard firstName="Jason" lastName="Lerit" />
+                <Teamcard firstName="Jomar" lastName="Clado" />
             </div>
         </div>
 
