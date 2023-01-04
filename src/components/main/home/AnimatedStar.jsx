accdc5b22a73c185c52bd1286db158696cc32326
jomar567 <cladojomar881@gmail.com>
2023-01-04 09:39:49 +0800

    fix bug

--- src/components/main/home/AnimatedStar.jsx
+++ src/components/main/home/AnimatedStar.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 const AnimatedStar = () => {
   useEffect(() => {
     animatedStars();
-  });
+  },[]);
   const generateRandomColor = () => {
     var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
     return randomColor;
