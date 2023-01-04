f8434bf1f2583573aab1a13f0c8ff6a2d104dd77
jasonlerit <jasonlerit077@gmail.com>
2023-01-04 13:10:42 +0800

    updated about page

--- src/pages/main/about.jsx
+++ src/pages/main/about.jsx
@@ -1,13 +1,11 @@
 import TeamSection from "../../components/main/about/TeamSection";
 import TeamCard from "../../components/main/about/TeamCard";
+
 const AboutPage = () => {
+
   return (
     <>
-      <h1 className="text-dark">ABOUT</h1>
-   
-      {/* Teams Section */}
-         <TeamSection/>
-       {/* Team Card */}
+      <TeamSection/>
       <TeamCard/>
     </>
   );
