e324c0efc00d973830e0d70a1863ed3f4f5f34fb
jasonlerit <jasonlerit077@gmail.com>
2023-01-05 19:33:44 +0800

    moved routing to app

---
+++ src/App.jsx
@@ -0,0 +1,128 @@
+
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js'
+import './assets/sass/index.scss';
+
+/* Home */
+import Layout from './components/layout/Layout';
+import HomePage from './pages/main/home'
+import PlanetsPage from './pages/main/planets';
+import AboutPage from './pages/main/about';
+
+/* Verify */
+import VerifyPage from './pages/verify/verify';
+
+/* Quiz */
+import QuizLayout from './components/layout/QuizLayout';
+import QuizPage from './pages/quiz/quiz';
+import RegisterPage from './pages/quiz/register';
+import LoginPage from './pages/quiz/login';
+import ForgotPasswordPage from './pages/quiz/forgot_password';
+import ResetPasswordPage from './pages/quiz/reset_password';
+
+/* User */
+import UserLayout from './components/layout/UserLayout';
+import DashboardPage from './pages/user/dashboard';
+import LeaderboardPage from './pages/user/leaderboard';
+import UserQuizPage from './pages/user/quiz'
+import SettingsPage from './pages/user/settings'
+
+/* Not found */
+import PageNotFound from './pages/404';
+import ProfilePage from './pages/user/profile';
+
+const router = createBrowserRouter([
+    /* Main */
+    {
+        element: <Layout />,
+        children: [
+        {
+            path: "",
+            element: <HomePage />,
+        },
+        {
+            path: "planets",
+            element: <PlanetsPage />,
+        },
+        {
+            path: "about",
+            element: <AboutPage />,
+        },
+        ]
+    },
+    /* Verify */
+    {
+        path: "verify",
+        element: <VerifyPage />
+    },
+    /* Quiz */
+    {
+        path: "quiz",
+        element: <QuizLayout />,
+        children: [
+        {
+            path: "",
+            element: <QuizPage />
+        },
+        {
+            path: "register",
+            element: <RegisterPage />
+        },
+        {
+            path: "login",
+            element: <LoginPage />
+        },
+        {
+            path: "forgot-password",
+            element: <ForgotPasswordPage />
+        },
+        {
+            path: "reset-password",
+            element: <ResetPasswordPage />
+        }
+        ]
+    },
+    /* User */
+    {
+        path: "user",
+        element: <UserLayout />,
+        children: [
+        {
+            path: "dashboard",
+            element: <DashboardPage />
+        },
+        {
+            path: "leaderboard",
+            element: <LeaderboardPage />
+        },
+        {
+            path: "quiz",
+            element: <UserQuizPage />
+        },
+        {
+            path: "settings",
+            element: <SettingsPage />
+        },
+        {
+            path: "profile",
+            element: <ProfilePage />
+        }
+        ]
+    },
+    /* Not found */
+    {
+        path: "*",
+        element: <PageNotFound />
+    }
+])
+
+const App = () => {
+    return <RouterProvider router={router} />
+}
+
+export default App
