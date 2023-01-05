2df34d62132bf5503a766c1f0f724a79cd835081
jasonlerit <jasonlerit077@gmail.com>
2023-01-05 22:37:50 +0800

    updated user profile

--- src/components/user/profile/UserProfile.jsx
+++ src/components/user/profile/UserProfile.jsx
@@ -154,10 +154,22 @@ const UserProfile = () => {
                             <div className="d-flex flex-column flex-md-row justify-content-center gap-2">
                                 {
                                     loading
-                                        ? <button className="btn btn-lg btn-quiz px-5" disabled>Loading...</button>
-                                        : <button className="btn btn-lg btn-quiz px-5" onClick={saveProfile}>SAVE</button>
+                                        ? (
+                                            <button className="btn btn-lg btn-quiz px-5" disabled>
+                                                Loading...
+                                            </button>
+                                        )
+                                        : (
+                                            <div className="d-flex flex-column flex-md-row justify-content-center gap-2">
+                                                <button className="btn btn-lg btn-quiz px-5" onClick={saveProfile}>
+                                                    SAVE
+                                                </button>
+                                                <button className="btn btn-lg btn-outline-dark px-5" onClick={() => setIsEditing(false)}>
+                                                    CANCEL
+                                                </button>
+                                            </div>
+                                        )
                                 }
-                                <button className="btn btn-lg btn-outline-dark px-5" onClick={() => setIsEditing(false)}>CANCEL</button>
                             </div>
                         )
                     }
