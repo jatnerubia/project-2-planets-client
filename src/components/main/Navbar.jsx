765e97a6ed6e9fe0b9982c66d354e4c8351c9e34
jomar567 <cladojomar881@gmail.com>
2023-01-06 19:55:03 +0800

    nav refix

--- src/components/main/Navbar.jsx
+++ src/components/main/Navbar.jsx
@@ -2,44 +2,44 @@ import { NavLink } from "react-router-dom"
 
 const Navbar = () => {
     return (
-        <nav className="position-absolute w-100 px-4 navbar navbar-expand-lg navbar-dark d-flex justify-content-between">
-            {/* Logo */}
-            <NavLink className="navbar-brand" to="/">
+        <nav className="position-absolute w-100 px-4 navbar navbar-expand-lg navbar-dark">
+            <div className="container">
+              {/* Logo */}
+              <NavLink className="navbar-brand" to="/">
                 <img src={require('../../assets/img/logo1.png')}
                     width="50"
                     height="50"
                     className="d-inline-block align-text-top"
                     alt="Logo" />
-            </NavLink>
-
-            {/* Toggle button */}
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
+              </NavLink>
+              <button className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbar">
                 <span className="navbar-toggler-icon"></span>
-            </button>
-
-            <div id="navbarSupportedContent" className="collapse navbar-collapse d-lg-flex justify-content-between">
-                <ul className="navbar-nav">
-                    <li className="nav-item px-2">
-                        <NavLink to="" className="nav-link">
-                            Home
-                        </NavLink>
-                    </li>
-                    <li className="nav-item px-2">
-                        <NavLink to="/planets" className="nav-link">
-                            Planets
-                        </NavLink>
-                    </li>
-                    <li className="nav-item px-2">
-                        <NavLink to="/about" className="nav-link">
-                            About
-                        </NavLink>
-                    </li>
-                </ul>
-                <div>
+              </button>
+              <div className="col-4 collapse navbar-collapse justify-content-between px-md-4" id="navbar">
+                <ul className="navbar-nav m-auto">
+                      <li className="nav-item px-2">
+                          <NavLink to="" className="nav-link text-white">
+                              Home
+                          </NavLink>
+                      </li>
+                      <li className="nav-item px-2">
+                          <NavLink to="/planets" className="nav-link text-white">
+                              Planets
+                          </NavLink>
+                      </li>
+                      <li className="nav-item px-2">
+                          <NavLink to="/about" className="nav-link text-white">
+                              About
+                          </NavLink>
+                      </li>
+                  </ul>
+                  <div>
                     <NavLink to="/quiz" className="btn btn-light py-2 px-4 custom-button">Quiz Game</NavLink>
-                </div>
+                  </div>
+              </div>
             </div>
         </nav>
     )
--- src/components/quiz/QuizNavbar.jsx
+++ src/components/quiz/QuizNavbar.jsx
@@ -13,34 +13,36 @@ const QuizNavbar = () => {
 
   return (
     <nav className="px-4 container-fluid navbar w-100 navbar-expand-md d-flex justify-content-between align-items-center">
-      <NavLink className="navbar-brand" to="/">
-        <img src={require('../../assets/img/logo1.png')} width="50" height="50" className="d-inline-block align-text-top" alt="Logo" />
-      </NavLink>
-      {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar"> */}
-      <div className={`navbar-toggler toggle ${activeCheck}`}
-          onClick={handleClick}
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbar">
-        <span></span>
-        <span></span>
-        <span></span>
-        <span></span>
-      </div>
-      {/* </button> */}
-      <div id="navbar" className="collapse navbar-collapse d-lg-flex justify-content-end">
-        <span>
-          <NavLink to="/quiz/register">
-            <button className="btn btn-quiz btn-sm mx-2 px-5 py-2">
-                Register
-            </button>
-          </NavLink>
-          <NavLink to="/quiz/login">
-            <button className="btn btn-quiz btn-sm mx-2 px-5 py-2">
-                Login
-            </button>
-          </NavLink>
-        </span>
+      <div className="container">
+        <NavLink className="navbar-brand" to="/">
+          <img src={require('../../assets/img/logo1.png')} width="50" height="50" className="d-inline-block align-text-top" alt="Logo" />
+        </NavLink>
+        {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar"> */}
+        <div className={`navbar-toggler toggle ${activeCheck}`}
+            onClick={handleClick}
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbar">
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        {/* </button> */}
+        <div id="navbar" className="collapse navbar-collapse d-lg-flex justify-content-end">
+          <span>
+            <NavLink to="/quiz/register">
+              <button className="btn btn-quiz btn-sm mx-2 px-5 py-2">
+                  Register
+              </button>
+            </NavLink>
+            <NavLink to="/quiz/login">
+              <button className="btn btn-quiz btn-sm mx-2 px-5 py-2">
+                  Login
+              </button>
+            </NavLink>
+          </span>
+        </div>
       </div>
     </nav>
   )
