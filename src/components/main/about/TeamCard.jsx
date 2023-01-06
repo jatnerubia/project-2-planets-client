5d727332c904eca81470495d899fd2ca76b1f392
jomar567 <cladojomar881@gmail.com>
2023-01-06 18:36:48 +0800

    team section fix

--- src/components/main/about/TeamCard.jsx
+++ src/components/main/about/TeamCard.jsx
@@ -1,23 +1,25 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { brands } from '@fortawesome/fontawesome-svg-core/import.macro'
 
-const TeamCard = ({ firstName, lastName }) => {
+const TeamCard = ({ firstName, lastName, image }) => {
     return (
         <div className="col-md-6 pb-5">
             <div className="card position-relative" style={{ width: "15rem;" }} >
-                <img src="https://i.ibb.co/2c362Xy/Untitled-design-4.png" className="position-absolute img-fluid top-0 start-50 translate-middle mb-5  d-block" alt="person" style={{ height: "50%", float: "auto" }} />
+                <img src={image} className="shadow bg-white position-absolute img-fluid top-0 start-50 translate-middle mb-5  d-block" alt="person" style={{ float: "auto" }} />
                 <div className="card-body mt-5 pt-5   text-center ">
-                    <h5 className="text-black">
+                    <h4 className="fw-bold text-black">
                         {firstName} {lastName}
-                    </h5>
+                    </h4>
                     <p className="text-black">Web Developer</p>
                     <li className="text-black list-unstyled">
-                        <FontAwesomeIcon className="m-1" icon={brands('facebook')} />
-                        <FontAwesomeIcon className="m-1" icon={brands('linkedin')} />
-                        <FontAwesomeIcon className="m-1" icon={brands('twitter')} />
+                        <FontAwesomeIcon className="m-1 fs-3 text-primary" icon={brands('facebook')} />
+                        <FontAwesomeIcon className="m-1 fs-3 text-primary" icon={brands('linkedin')} />
+                        <FontAwesomeIcon className="m-1 fs-3 text-primary" icon={brands('twitter')} />
                     </li>
-                    <button className="bg-black text-white btn rounded-pill m-2">FOLLOW</button>
-                    <button className="border-dark text-black btn rounded-pill">HIRE ME</button>
+                    <div className="button-group pt-5 d-flex justify-content-evenly">
+                      <button className="bg-black text-white btn rounded-pill px-4">FOLLOW</button>
+                      <button className="border-dark text-black btn rounded-pill px-4">HIRE ME</button>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/main/about/TeamSection.jsx
+++ src/components/main/about/TeamSection.jsx
@@ -4,12 +4,14 @@ import Teamcard from "./TeamCard"
 const TeamSection = () => {
     return (
         <div className="container py-5">
-            <SectionHeading name="MEET THE TEAM" />
-            <div className="row gy-5 pt-5">
-                <Teamcard firstName="Alan" lastName="Demol" />
-                <Teamcard firstName="Angela" lastName="Morales" />
-                <Teamcard firstName="Jason" lastName="Lerit" />
-                <Teamcard firstName="Jomar" lastName="Clado" />
+            <SectionHeading className="pt-5 mt-5" name="ABOUTS US" classname="team"/>
+            <div className="team-wrapper">
+              <div className="row gy-5 pt-5">
+                <Teamcard firstName="Alan" lastName="Demol" image={require('../../../assets/img/AlanDemol.jpg')}/>
+                <Teamcard firstName="Angela" lastName="Morales" image={require('../../../assets/img/AngelaMorales.jpg')}/>
+                <Teamcard firstName="Jason" lastName="Lerit" image="https://imgs.search.brave.com/MeX0FJdaur-HM7RpycRW3dHLdPTppvyhw-iVqXRrjFQ/rs:fit:650:689:1/g:ce/aHR0cHM6Ly93d3cu/cG5na2V5LmNvbS9w/bmcvZnVsbC8xNS0x/NTAzODNfdHJ1bXAt/dGhpbmstdHJ1bXAt/dGhvbmstZW1vamkt/aG1tLnBuZw"/>
+                <Teamcard firstName="Jomar" lastName="Clado" image={require('../../../assets/img/JomarClado.jpeg')}/>
+              </div>
             </div>
         </div>
     )
