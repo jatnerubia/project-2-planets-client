3ceda041010eeef37ebf2f73613019279243b337
jasonlerit <jasonlerit077@gmail.com>
2023-01-18 07:59:16 +0800

    updated team section

--- src/components/main/about/TeamSection.jsx
+++ src/components/main/about/TeamSection.jsx
@@ -11,7 +11,7 @@ const TeamSection = () => {
             <div className="row gy-5 pt-5">
               <Teamcard firstName="Alan" lastName="Demol" image={require('../../../assets/img/AlanDemol.jpg')}/>
               <Teamcard firstName="Angela" lastName="Morales" image={require('../../../assets/img/AngelaMorales.jpg')}/>
-              <Teamcard firstName="Jason" lastName="Lerit" image="https://imgs.search.brave.com/MeX0FJdaur-HM7RpycRW3dHLdPTppvyhw-iVqXRrjFQ/rs:fit:650:689:1/g:ce/aHR0cHM6Ly93d3cu/cG5na2V5LmNvbS9w/bmcvZnVsbC8xNS0x/NTAzODNfdHJ1bXAt/dGhpbmstdHJ1bXAt/dGhvbmstZW1vamkt/aG1tLnBuZw"/>
+              <Teamcard firstName="Jason" lastName="Lerit" image={require('../../../assets/img/JasonLerit.jpg')}/>
               <Teamcard firstName="Jomar" lastName="Clado" image={require('../../../assets/img/JomarClado.jpeg')}/>
             </div>
           </div>
